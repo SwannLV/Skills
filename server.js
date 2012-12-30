f821abac55fe314bd9127ce60314301f43bc2bfb
SwannLV <le.vigouroux.swann@gmail.com>
2012-12-30 10:02:28 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express').createServer();
 var port = process.env.PORT || 3000;
 app.listen(port);
 
-app.get('/', function(req, res){res.sendfile(__dirname + '/index.html');}); 
+app.get('/', function(req, res){res.sendfile(__dirname + '/Index.html');}); 
 app.get('/d3/d3.js', function(req, res){res.sendfile(__dirname + '/d3/d3.js');});
 app.get('/d3/d3.geom.js', function(req, res){res.sendfile(__dirname + '/d3/d3.geom.js');});
 app.get('/style.css', function(req, res){res.sendfile(__dirname + '/style.css');});
